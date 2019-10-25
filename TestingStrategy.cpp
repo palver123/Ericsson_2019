bbe20cc4244ebc0988fa277543d80d73770cc8c2
Levente Koncz <levente.koncz@sapagroup.com>
2019-10-25 18:41:54 +0200

    Only create data packet on slots that are not closed.

--- TestingStrategy.cpp
+++ TestingStrategy.cpp
@@ -35,7 +35,7 @@ std::string TestingStrategy::step(Reader &turnData)
         }
 
         for (auto slot = 0; slot < NSLOTS; slot++)
-            if (!slotTaken[slot])
+            if (!slotTaken[slot] && turnData.routerBits[MY_ROUTER][slot])
                 return fmt::format("CREATE {} {}", slot, maxPckIdx);
     }
 
