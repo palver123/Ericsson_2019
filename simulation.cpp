7aea0297c7de1526facfda3a3177c119667a4b45
Levente Koncz <levente.koncz@sapagroup.com>
2019-11-09 01:30:34 +0100

    Fixed skeleton of simulation.h based on the docs

--- simulation.cpp
+++ simulation.cpp
@@ -1,11 +1,35 @@
 #include "simulation.h"
 
-NetworkState simulate(const NetworkState& state, const CreateCommand& cmd)
-{
-    throw std::runtime_error("Not implemented yet!");
+using namespace std;
+
+void remove_arrived_packets(NetworkState& state)
+{    
 }
 
-NetworkState simulate(const NetworkState& state, const MoveCommand& cmd)
+void add_new_packets(NetworkState& state, const vector<CreateCommand>& commands)
+{    
+}
+
+void apply_move_commands(NetworkState& state, const vector<MoveCommand>& commands)
+{    
+}
+
+bool step_packets(NetworkState& state, vector<bool>& isPacketFrozen)
 {
-    throw std::runtime_error("Not implemented yet!");
+    return false;
+}
+
+NetworkState simulate(const NetworkState& initialState, const vector<CreateCommand>& createCmds, const vector<MoveCommand>& moveCmds)
+{
+    auto retVal{ initialState };
+
+    remove_arrived_packets(retVal);
+    add_new_packets(retVal, createCmds);
+    apply_move_commands(retVal, moveCmds);
+
+    vector<bool> isPacketFrozen; // If a packet arrives in its destination it is 'frozen' until the next simulate call
+    fill_n(back_inserter(isPacketFrozen), retVal.dataPackets.size(), false);
+    while (step_packets(retVal, isPacketFrozen)) { }
+
+    return retVal;
 }
