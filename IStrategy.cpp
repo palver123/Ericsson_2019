d6746e89952f0ec7057ca3b792a1d18af1248e11
Levente Koncz <levente.koncz@sapagroup.com>
2019-11-21 00:33:07 +0100

    Removed old, unused code

--- IStrategy.cpp
+++ IStrategy.cpp
@@ -1,28 +1,13 @@
 #include "IStrategy.h"
 #include "Players.h"
-#include <random>
 #include <array>
 
 using namespace std;
-
-static std::mt19937_64 my_random{std::random_device{}()};
 
 std::string IStrategy::step(NetworkState& turnData, const GameContext& ctx) {
     static auto tc = 0;
     std::cerr << "Turn " << tc++ << std::endl;
     return "PASS";
-}
-
-int IStrategy::getRandom(const int from, const int to_exclusive)
-{
-    const auto d = static_cast<unsigned long long>(to_exclusive - from);
-    return static_cast<int>(my_random()  % d) + from;
-}
-
-ll IStrategy::getRandomLL(const ll from, const ll to_exclusive)
-{
-    const auto d = static_cast<unsigned long long>(to_exclusive - from);
-    return static_cast<ll>(my_random()  % d) + from;
 }
 
 void IStrategy::stepPre(const NetworkState& turnData, const GameContext& ctx)
